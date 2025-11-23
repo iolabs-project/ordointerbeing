d4b1e7e9855549af896e7ee674ab53efc9977b27
albertirgi <irgialbert@gmail.com>
2025-11-23 17:46:00 +0700

    add: favicon ant title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,11 +3,17 @@ import "../styles/main.scss";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+export const metadata = {
+  title: "Komunitas Zen Plum Village",
+  description: "Komunitas Zen Plum Village",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/assets/navbar-logo.png" sizes="any" />
+        <link rel="apple-touch-icon" href="/assets/navbar-logo.png" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
